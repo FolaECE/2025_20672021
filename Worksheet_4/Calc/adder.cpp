dd10f6ac153c7fab65fdbeeac854469059e659ed
FolaECE <eeyoa14@nottingham.ac.uk>
2026-02-13 13:44:59 +0000

    Add .gitignore

---
+++ Worksheet_4/Calc/adder.cpp
@@ -0,0 +1,6 @@
+#include "adder.h"
+#define mathsEXPORTS
+
+int add(int a, int b) {
+    return a + b;
+}
---
+++ Worksheet_5/adder.h
@@ -0,0 +1,18 @@
+// adder.h
+
+#ifndef MATHSLIBADDERH
+#define MATHSLIBADDERH
+
+/** @file
+ * This file contains the declarations of all exported functions in the maths library.
+ */
+
+/** Addition function 
+ * 2 numbers are added together in this function
+ * @param a is the first integer added
+ * @param b is the second integer added
+ * @return the addition of a and b 
+ */
+int add(int a, int b);
+
+#endif
