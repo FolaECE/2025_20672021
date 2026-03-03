2f731346b0852e7e4a86c9b31a890ff264a0e81d
FolaECE <eeyoa14@nottingham.ac.uk>
2026-03-03 07:24:53 +0000

    Completed Worksheet 6 Exercise 1

--- Worksheet_6/Project/mainwindow.cpp
+++ Worksheet_6/Project/mainwindow.cpp
@@ -1,14 +1,23 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    
+}
+
+void MainWindow::handleButton() {
+    QMessageBox msgBox;
+    msgBox.setText("Add button was clicked");
+    msgBox.exec();
 }
 
 MainWindow::~MainWindow()
 {
+    connect(ui->pushButton, &QPushButton::released, this, &MainWindow::handleButton);
     delete ui;
 }
