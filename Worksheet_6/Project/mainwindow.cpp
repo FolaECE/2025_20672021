a30d1a6a12d2d22c44dda83a80d25a681590048e
FolaECE <eeyoa14@nottingham.ac.uk>
2026-03-03 08:34:59 +0000

    Completed Worksheet 6 Exercise 4

--- Worksheet_6/Project/mainwindow.cpp
+++ Worksheet_6/Project/mainwindow.cpp
@@ -7,12 +7,35 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    connect(ui->pushButton,
-        &QPushButton::released,
-        this,
-        &MainWindow::handleButton); 
 
-    connect( this, &MainWindow::statusUpdateMessage, ui->statusbar, &QStatusBar::showMessage );
+    partList = new ModelPartList("PartsList", this);
+    ui->treeView->setModel(this->partList);
+
+    ModelPart* rootItem = this->partList->getRootItem();
+
+    /* Add 3 top level items */
+    for (int i = 0; i < 3; i++) {
+        /* Create strings for both data columns */
+        QString name = QString("TopLevel %1").arg(i);
+        QString visible("true");
+
+        /* Create child item */
+        ModelPart* childItem = new ModelPart({ name, visible });
+
+       
+        rootItem->appendChild(childItem);
+
+       
+        for (int j = 0; j < 5; j++) {
+            QString name = QString("Item %1,%2").arg(i).arg(j);
+            QString visible("true");
+
+            ModelPart* childChildItem = new ModelPart({ name, visible });
+
+
+            childItem->appendChild(childChildItem);
+        }
+    }
 }
 
 void MainWindow::handleButton() {
--- Worksheet_6/Project/mainwindow.h
+++ Worksheet_6/Project/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "ModelPart.h"
+#include "ModelPartList.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -25,5 +27,6 @@ public slots:
 
 private:
     Ui::MainWindow *ui;
+    ModelPartList* partList;
 };
 #endif // MAINWINDOW_H
